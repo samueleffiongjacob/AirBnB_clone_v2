8ee17e649704bd8c5c189644fe1fda085f146744
samueleffiongjacob <samuelefffiongjacob@gmail.com>
2024-08-15 03:53:42 +0100

    jj

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,10 +2,10 @@
 """A module for Fabric script that generates a .tgz archive."""
 import os
 from datetime import datetime
-from fabric import task
+from fabric.api import local, task
 
 @task
-def do_pack(c):
+def do_pack():
     """Archives the static files."""
     if not os.path.exists("versions"):
         os.makedirs("versions")
@@ -15,7 +15,7 @@ def do_pack(c):
 
     try:
         print("Packing web_static to {}".format(output))
-        c.local("tar -cvzf {} web_static".format(output))
+        local("tar -cvzf {} web_static".format(output))
         size = os.stat(output).st_size
         print("web_static packed: {} -> {} Bytes".format(output, size))
         return output
