62c8becfa1b70732268430c17b042027786643da
samueleffiongjacob <samuelefffiongjacob@gmail.com>
2024-08-15 02:46:49 +0100

    i done try

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,21 @@
-#!/usr/bin/python3
-"""A module for Fabric script that generates a .tgz archive."""
-import os
-from datetime import datetime
 from fabric import task
+import os
 
 @task
-def do_pack(c):
-    """Archives the static files."""
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
+def do_pack(ctx):
+    """
+    Generates a .tgz archive from the contents of the web_static folder.
+    """
+    local_time = time.strftime("%Y%m%d%H%M%S")
+    archive_name = f"web_static_{local_time}.tgz"
+    archive_path = f"versions/{archive_name}"
 
-    d_time = datetime.now().strftime("%Y%m%d%H%M%S")
-    output = "versions/web_static_{}.tgz".format(d_time)
-
-    try:
-        print("Packing web_static to {}".format(output))
-        c.local("tar -cvzf {} web_static".format(output))
-        size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, size))
-        return output
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
+    print(f"Packing web_static to {archive_path}")
+    if not os.path.exists('versions'):
+        os.makedirs('versions')
+    
+    result = ctx.run(f"tar -czvf {archive_path} web_static")
+    if result.ok:
+        print(f"web_static packed: {archive_path}")
+    else:
+        print("Packing failed")
