b826d06c0feb202e38292095f349b81d4139e63e
samueleffiongjacob <samuelefffiongjacob@gmail.com>
2024-08-15 02:59:49 +0100

    deploy

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,16 @@
-#!/usr/bin/python3
-"""A module for Fabric script that generates a .tgz archive."""
+#!/usr/bin/env python3
 import os
 from datetime import datetime
 from fabric import task
 from invoke import run
+
 
 @task
 def do_pack(c):
     """Archives the static files."""
     if not os.path.isdir("versions"):
         os.mkdir("versions")
+    
     d_time = datetime.now()
     output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
         d_time.year,
@@ -19,13 +20,14 @@ def do_pack(c):
         d_time.minute,
         d_time.second
     )
+    
     try:
         print("Packing web_static to {}".format(output))
         run(f"tar -cvzf {output} web_static")
         size = os.stat(output).st_size
         print("web_static packed: {} -> {} Bytes".format(output, size))
     except Exception as e:
-        print(f"Failed to pack: {e}")
+        print(f"Failed to pack web_static: {e}")
         output = None
+    
     return output
-dada
